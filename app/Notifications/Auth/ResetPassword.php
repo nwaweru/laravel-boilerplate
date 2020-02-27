6204764e3bd488f17ab92c2d8a50175268afa8aa
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-02-27 19:36:52 +0300

    wip

---
+++ app/Notifications/Auth/ResetPassword.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications\Auth;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPassword extends Notification
+{
+    use Queueable;
+
+    /**
+     * The user.
+     *
+     * @var
+     */
+    public $user;
+
+    /**
+     * The password reset token.
+     *
+     * @var
+     */
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $token)
+    {
+        $this->user = $user;
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)->subject('Reset Password')
+            ->markdown('emails.auth.passwords.reset', [
+                'user' => $this->user,
+                'token' => $this->token,
+            ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ resources/views/emails/auth/passwords/reset.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+Hi {{ $user->first_name }},
+
+We have received a password reset for your account.
+
+@component('mail::button', ['url' => route('password.reset', ['token' => $token, 'email' => $user->email])])
+Reset Password
+@endcomponent
+
+This password reset link will expire in {{ config('auth.passwords.users.expire') }} minutes.
+
+If you did not request a password reset, no further action is required.
+
+Regards,<br>
+The {{ config('app.name') }} Team.
+@endcomponent
