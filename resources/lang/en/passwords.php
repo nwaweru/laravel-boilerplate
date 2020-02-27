761b7f291da1263a5f00c0e5e17e9b46385cb188
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-02-27 19:47:36 +0300

    wip

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'reset' => 'Your password has been reset.',
+    'sent' => 'Check your email for the password reset link.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "We can't find an account with e-mail address.",
 
 ];
