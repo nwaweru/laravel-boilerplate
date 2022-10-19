ff46f0cad27741058c733ef1924c8072698eefe8
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-10-19 16:12:34 +0300

    set model strictness

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Model::shouldBeStrict($this->app->isLocal());
     }
 }
