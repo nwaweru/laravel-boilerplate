5b2ef77ad13d0ab881712b63b214503d6983093f
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-11-10 08:57:57 +0300

    disabled model strictness

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Model::shouldBeStrict($this->app->isLocal());
+        // Model::shouldBeStrict($this->app->isLocal());
     }
 }
